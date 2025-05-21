c8b95a699be45c311c0c8e330193933cf360d576
AKhoriby <ahmedelkhoriby010@gmail.com>
2025-05-21 04:33:32 +0300

    feat: implement user management features including CRUD operations and update DTO

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,47 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
-  @Post('signup')
-  signup(@Body() body: CreateUserDto) {
-    console.log(body);
-    return body;
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() body: CreateUserDto) {
+    return this.usersService.create(body.email, body.password);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    const user = this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
+  }
+
+  @Delete(':id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
