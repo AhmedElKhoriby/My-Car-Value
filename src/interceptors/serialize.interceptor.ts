65009207b73ac3120bac1278593d6816231080f3
AKhoriby <ahmedelkhoriby010@gmail.com>
2025-05-23 18:01:38 +0300

    feat: add serialization interceptor and UserDto for user responses

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,38 @@
+import {
+  ExecutionContext,
+  CallHandler,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+import { plainToInstance } from 'class-transformer';
+
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
+// Factory Function to use the interceptor that return a decorator
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    // Run something before a request is handled by the request handler
+    console.log("I'm running before the handler", context);
+
+    return next.handle().pipe(
+      map((data: any) => {
+        // Run something before the response is sent out
+        // console.log("I'm running before the response is sent out", data);
+
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,7 +11,10 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
